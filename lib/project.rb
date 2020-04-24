1d92ece48368006b9647dba877638de93614d6a1
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 18:16:45 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,8 +8,8 @@ class Project
     @backers = []
   end
   
-  def add_backer(backer_obj)
-    @backers << backer_obj
+  def add_backer(backer)
+    @backers << backer
     backer.backed_project(self) unless backed_project.include?(self)
   end
 end
